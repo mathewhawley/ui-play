59b56caed68058e073c08054549dc24406f5f241
Mathew Hawley <mathew@mathewhawley.com>
2016-11-09 20:43:53 +0000

    Set up class, attached simple click listener

--- sidenav/script.js
+++ sidenav/script.js
@@ -1,1 +1,19 @@
-console.log('Hello, world!');
+class SideNav {
+  constructor() {
+    this.sideNavTrigger = document.querySelector('.js-header__menu-button');
+
+    this.openSideNav = this.openSideNav.bind(this);
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    this.sideNavTrigger.addEventListener('click', this.openSideNav);
+  }
+
+  openSideNav() {
+    console.log('clicked');
+  }
+}
+
+window.addEventListener('load', () => new SideNav());
